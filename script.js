dc02119fd5946afa64bcae1dc730977a1e9ffc5c
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-04-10 17:10:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,12 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
+	for(let i=0; i<encodedStr.length; i++)
+		{
+			let charCode=encodedStr.charCodeAt(i);
+			let updatedCharCode=(charCode+13)%26;
+			decodedArr.push(string.fromCharCode(updatedCharCode));
+		}
   return ;//return decodedArr
 }
 
