3ba2e6e595670bbade0ccd68d3c3f3e56b0c8858
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-04-10 17:12:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function rot13(encodedStr){
 			let updatedCharCode=(charCode+13)%26;
 			decodedArr.push(string.fromCharCode(updatedCharCode));
 		}
-  return ;//return decodedArr
+  return decodedArr;
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
