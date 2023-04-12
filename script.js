5b61ff7da5267ddfe1104f13b6f7754f105c6ae4
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-04-12 14:51:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,18 +16,13 @@ function rot13(encodedStr){
 	for(let i=0; i<encodedStr.length; i++)
 		{
 			let char=encodedStr[i];
-			if(char.match(/[A-Z]/))
-				{
-					let charCode=encodedStr.charCodeAt(i)-65;   //-65 for capital letters and -97 for small letters
-					let updatedCharCode=(charCode+13)%26;
-					decodedArr.push(String.fromCharCode(updatedCharCode+65));
-				}
+			decodedArr.push(lookup[char]);
 		}
-  return decodedArr;
+  return decodedArr.join('');
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("SERR YBIR? NPPVBWBO"));
 
-module.exports = rot13;
+// module.exports = rot13;
